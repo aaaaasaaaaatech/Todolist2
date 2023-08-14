05fcae02d783a663865e9f2c05e5ca0db79f45c4
aaaaasaaaaatech <132049862+aaaaasaaaaatech@users.noreply.github.com>
2023-08-15 00:06:52 +0900

    service

--- src/main/java/com/example/todolist/form/TodoData.java
+++ src/main/java/com/example/todolist/form/TodoData.java
@@ -1,9 +1,48 @@
 package com.example.todolist.form;
 
+import java.text.SimpleDateFormat;
+
+import com.example.todolist.entity.Todo;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class TodoData {
-	pri
+	private Intger id;
+	
+	@NotBlank(message="件名を入力してください")
+	private String title;
+	
+	@NotNull(message="重要度を選択してください")
+	private Integer importance;
+	
+	@Min(value=0,message="緊急度を選択してください")
+	private Integer urgency;
+	
+	public Todo toEntity() {
+		Todo todo =newTodo();
+		todo.setId(id);
+		todo.setImportance(importance);
+		todo.setUrgency(urgency);
+		todo.setDone(done);
+		
+		SimpleDateFormat sdFormat = new SimpleDataFormat("yyyy-MM-dd");
+		long ms;
+		try {
+			ms =sdFormat.parse(deadline).getTime();
+			todo.setDeadline(new Date(ms));
+		}catch(ParseException e) {
+			todo.setDeadline(null);
+		}
+		
+		return todo;
+		
+	}
+	
+	
+	
 
 }
--- src/main/java/com/example/todolist/service/TodoService.java
+++ src/main/java/com/example/todolist/service/TodoService.java
@@ -1,5 +1,29 @@
 package com.example.todolist.service;
 
+import org.springframework.stereotype.Service;
+import org.springframework.validation.FieldError;
+
+import com.example.todolist.form.TodoData;
+
+@Service
 public class TodoService {
+	
+	public boolean is Vaild(TodoData todoData,BindingResult result) {
+		boolean ans = true;
+		
+		String title = todoData.getTitle();
+		if(title != null&& !title.equals("")) {
+			boolean is AllDoubleSpace = true;
+			for(int i = o;i<title.length();i++) {
+				if(title.charAt(i)!=' ') {
+					isAllDoubleSpace=false;
+					break;
+				}
+			}
+			if(isAllDoubleSpace) {
+				FieldError fieldError=new FieldError
+			}
+		}
+	}
 
 }
