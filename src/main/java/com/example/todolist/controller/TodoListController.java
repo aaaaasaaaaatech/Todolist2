5f960da94e8be7dc5c9f5a7f056359c97bc05c4d
aaaaasaaaaatech <132049862+aaaaasaaaaatech@users.noreply.github.com>
2023-08-15 00:50:23 +0900

    LIst

--- src/main/java/com/example/todolist/controller/TodoListController.java
+++ src/main/java/com/example/todolist/controller/TodoListController.java
@@ -7,51 +7,59 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.method.support.ModelAndViewContainer;
 import org.springframework.web.servlet.ModelAndView;
 import com.example.todolist.entity.Todo;
 import com.example.todolist.form.TodoData;
 import com.example.todolist.repository.TodoRepository;
+import com.example.todolist.service.TodoService;
 import lombok.AllArgsConstructor;
 
 @Controller
-@AllArgsConstructor // ②
+@AllArgsConstructor
 public class TodoListController {
 	private final TodoRepository todoRepository;
-	private final TodoService todoService;
+	private final TodoService todoService; // Todolist2 で追加
+// ToDo 一覧表示(Todolist で追加)
 
 	@GetMapping("/todo")
 	public ModelAndView showTodoList(ModelAndView mv) {
-		// 一覧を検索して表示する
 		mv.setViewName("todoList");
-		List<Todo> todoList = todoRepository.findAll(); // ③
-		mv.addObject("todoList", todoList); // ④
+		List<Todo> todoList = todoRepository.findAll();
+		mv.addObject("todoList", todoList);
 		return mv;
 	}
-	
+
+// ToDo 入力フォーム表示(Todolist2 で追加)
+// 【処理 1 】 ToDo 一覧画面(todoList.html)で[新規追加]リンクがクリックされたとき
 	@GetMapping("/todo/create")
 	public ModelAndView createTodo(ModelAndView mv) {
-		mv.setViewName("todoForm");
-		mv.addObject("todoData",new TodoData());
+		mv.setViewName("todoForm"); // ①
+		mv.addObject("todoData", new TodoData()); // ②
 		return mv;
 	}
-	
+
+// ToDo 追加処理(Todolist2 で追加)
+// 【処理 2 】 ToDo 入力画面(todoForm.html)で[登録]ボタンがクリックされたとき
 	@PostMapping("/todo/create")
-	public ModelAndView createTodo(@ModelAttribute @Validated TodoData todoData,
-			BindingResult result,
-			ModelAndView mv) {
-		boolean is Valid = todoService.isValid(todoData,result);
-		if(!result.hasErrors()&& isValid) {
-			Todo todo =todoData.toEntity();
+	public ModelAndView createTodo(@ModelAttribute @Validated TodoData todoData, // ③
+			BindingResult result, ModelAndView mv) {
+		// エラーチェック
+		boolean isValid = todoService.isValid(todoData, result); // ④
+		if (!result.hasErrors() && isValid) {
+			// エラーなし
+			Todo todo = todoData.toEntity(); // ⑤
 			todoRepository.saveAndFlush(todo);
 			return showTodoList(mv);
-			
-		}else {
-			mv.setViewName("todoForm")
+		} else {
+			// エラーあり
+			mv.setViewName("todoForm"); // ⑥
+			// mv.addObject("todoData", todoData);
 			return mv;
 		}
 	}
-	
+
+	// ToDo 一覧へ戻る(Todolist2 で追加)
+	// 【処理 3 】 ToDo 入力画面で[キャンセル登録]ボタンがクリックされたとき
 	@PostMapping("/todo/cancel")
 	public String cancel() {
 		return "redirect:/todo";
--- src/main/java/com/example/todolist/form/TodoData.java
+++ src/main/java/com/example/todolist/form/TodoData.java
@@ -1,48 +1,47 @@
 package com.example.todolist.form;
 
+import java.sql.Date;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
-
-import com.example.todolist.entity.Todo;
-
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import com.example.todolist.entity.Todo;
 import lombok.Data;
 
 @Data
 public class TodoData {
-	private Intger id;
-	
-	@NotBlank(message="件名を入力してください")
+	private Integer id;
+	@NotBlank(message = "件名を入力してください")
 	private String title;
 	
-	@NotNull(message="重要度を選択してください")
+	@NotNull(message = "重要度を選択してください")
 	private Integer importance;
 	
-	@Min(value=0,message="緊急度を選択してください")
+	@Min(value = 0, message = "緊急度を選択してください")
 	private Integer urgency;
 	
+	private String deadline;
+	private String done;
+
+	/**
+	 * 入力データから Entity を生成して返す
+	 */
 	public Todo toEntity() {
-		Todo todo =newTodo();
+		Todo todo = new Todo();
 		todo.setId(id);
+		todo.setTitle(title);
 		todo.setImportance(importance);
 		todo.setUrgency(urgency);
 		todo.setDone(done);
-		
-		SimpleDateFormat sdFormat = new SimpleDataFormat("yyyy-MM-dd");
+		SimpleDateFormat sdFormat = new SimpleDateFormat("yyyy-MM-dd");
 		long ms;
 		try {
-			ms =sdFormat.parse(deadline).getTime();
+			ms = sdFormat.parse(deadline).getTime();
 			todo.setDeadline(new Date(ms));
-		}catch(ParseException e) {
+		} catch (ParseException e) {
 			todo.setDeadline(null);
 		}
-		
 		return todo;
-		
 	}
-	
-	
-	
-
 }
